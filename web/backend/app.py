88422f1275e0ddcbec9c6f2376bbefc80fae7cd5
canitey <mohamedmed01345@gmail.com>
2024-10-29 21:00:21 +0300

    did something good

--- web/backend/app.py
+++ web/backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
 import time
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 count = 0
 
@@ -9,10 +12,13 @@ count = 0
 def event_stream():
     def generate():
         while True:
-            time.sleep(.5)
+            time.sleep(1)
             yield f"data: {count}\n\n"
 
-    return Response(generate(), 200, mimetype="text/event-stream")
+    return Response(generate(),
+                    200,
+                    mimetype="text/event-stream",
+                    )
 
 
 @app.get("/")
@@ -20,12 +26,8 @@ def home():
     return render_template("index.html")
 
 
-@app.get("/ss")
-def ss():
-    return "hehe"
-
-
 @app.get("/events")
+@cross_origin()
 def events():
     return event_stream()
 
