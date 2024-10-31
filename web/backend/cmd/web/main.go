7b9a0a0d4a3697b2bbbdadc837b80a4321ff2016
canitey <mohamedmed01345@gmail.com>
2024-10-31 23:47:02 +0300

    made better error handling and delivered the data to the front end

--- web/backend/cmd/web/main.go
+++ web/backend/cmd/web/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"gp-backend/cmd/models"
+	"gp-backend/models"
 	"log"
 	"net/http"
 
@@ -17,29 +16,64 @@ func main() {
 	mux.HandleFunc("GET /events", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("access-control-allow-origin", "*")
 		server.ServeHTTP(w, r)
-	},
-)
+	})
+
 	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
 		server.Publish("messages", &sse.Event{
 			Data: []byte("ping"),
 		})
 		log.Println("TRIGGER SENT")
-		fmt.Fprintln(w, server.Headers)
 	})
 
 	mux.HandleFunc("POST /sos", func(w http.ResponseWriter, r *http.Request) {
 		var data models.CarInfo
 		decoder := json.NewDecoder(r.Body)
+		
+		// handle prasing errors
 		if err := decoder.Decode(&data); err != nil {
 			log.Printf("Can't decode json body from [%v]", r.RemoteAddr)
 			msg := models.Msg{
-				Status: "FAIL",
-				Message: "Can't parse json",
+				Status: "fail",
+				Message: "JSON object is malformed",
 			}
-			encoder := json.NewEncoder(w)
-			encoder.Encode(msg)
+			JSONwriter(w, msg)
 			return
 		}
+
+
+		if data.UUID == "" {
+			log.Printf("Can't decode json body from [%v]", r.RemoteAddr)
+			msg := models.Msg{
+				Status: "fail",
+				Message: "provide UUID",
+			}
+			JSONwriter(w, msg)
+			return
+		} else if data.DriverStatus == "" {
+			log.Printf("Can't decode json body from [%v]", r.RemoteAddr)
+			msg := models.Msg{
+				Status: "fail",
+				Message: "provide driver_status",
+			}
+			JSONwriter(w, msg)
+			return
+		}
+
+		msg := models.Msg{
+			Status: "success",
+			Message: "success",
+		}
+		JSONwriter(w, msg)
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			log.Println("Couldn't generate cars' data")
+		}
+		
+
+		server.Publish("messages", &sse.Event{
+			Data: jsonData,
+		})
 	})
 
 
