81fdd21226ad10e54ab4738a5614104b2b3f110b
canitey <mohamedmed01345@gmail.com>
2024-11-26 16:51:40 +0200

    renamed a path and added first login path

--- web/backend/cmd/web/routes.go
+++ web/backend/cmd/web/routes.go
@@ -5,9 +5,10 @@ import "net/http"
 func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("POST /user/login", a.login)
 	mux.HandleFunc("GET /events", a.stream)
 	mux.HandleFunc("GET /ping", a.ping)
-	mux.HandleFunc("POST /sos", a.sos)
+	mux.HandleFunc("POST /cars/sos", a.sos)
 
 
 	return mux
