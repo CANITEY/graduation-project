442a9f5d90e33e8653ab8ee96fd028790dcfa93f
canitey <mohamedmed01345@gmail.com>
2024-11-29 17:18:22 +0200

    added a handler for the challenge post request

--- web/backend/cmd/web/routes.go
+++ web/backend/cmd/web/routes.go
@@ -6,6 +6,7 @@ func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /user/login", a.login)
+	mux.HandleFunc("POST /user/challenge", a.challenge)
 	mux.HandleFunc("GET /events", a.stream)
 	mux.HandleFunc("GET /ping", a.ping)
 	mux.HandleFunc("POST /cars/sos", a.sos)
