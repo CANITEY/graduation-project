0d570ff4ff372a0afc49650fe15ba9ed09ee0b4a
canitey <mohamedmed01345@gmail.com>
2024-11-30 14:33:55 +0200

    added a function that gets the challenge body

--- web/backend/cmd/web/challengeHandlers.go
+++ web/backend/cmd/web/challengeHandlers.go
@@ -1,3 +1,26 @@
 package main
 
+import (
+	"gp-backend/database"
+	"net/http"
+)
 
+func (a *application) challGetter(w http.ResponseWriter, r *http.Request) {
+	challUUID := r.PathValue("challUUID")
+	challenge, err := database.GetChallengeStr(a.db, challUUID)
+	if err != nil {
+		serverError(w, err)
+	}
+
+	jsonMsg := struct{
+		Status string `json:"status"`
+		Challenge string `json:"challenge"`
+	}{
+		Status: "success", 
+		Challenge: challenge,
+	}
+
+	if err := writeJSON(w, http.StatusOK, jsonMsg, nil); err != nil {
+		serverError(w, err)
+	}
+}
