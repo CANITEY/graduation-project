4064ec5a108d929f64746692beebf4932b69332e
canitey <mohamedmed01345@gmail.com>
2024-12-01 22:33:47 +0200

    changed the rearrangment of the routes

--- web/backend/cmd/web/routes.go
+++ web/backend/cmd/web/routes.go
@@ -5,12 +5,12 @@ import "net/http"
 func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /user/login", a.login)
-	mux.HandleFunc("POST /user/challenge", a.challenge) // this endpoint is used after the challenge was solved with the validator, to tell the front end to redirect to dashboard
 	mux.HandleFunc("GET /challenge/{challUUID}", a.challGetter)
-	mux.HandleFunc("POST /challenge/{challUUID}", a.challengeValidator)
 	mux.HandleFunc("GET /events", a.stream)
 	mux.HandleFunc("GET /ping", a.ping)
+	mux.HandleFunc("POST /user/login", a.login)
+	mux.HandleFunc("POST /user/challenge", a.challenge) // this endpoint is used after the challenge was solved with the validator, to tell the front end to redirect to dashboard
+	mux.HandleFunc("POST /challenge/{challUUID}", a.challengeValidator)
 	mux.HandleFunc("POST /cars/sos", a.sos)
 
 
