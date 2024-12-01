107ed76a08b0149aa2b577639759c4a71249c8b1
canitey <mohamedmed01345@gmail.com>
2024-12-01 22:56:05 +0200

    added a new route

--- web/backend/cmd/web/routes.go
+++ web/backend/cmd/web/routes.go
@@ -8,6 +8,7 @@ func (a *application) routes() http.Handler {
 	mux.HandleFunc("GET /challenge/{challUUID}", a.challGetter)
 	mux.HandleFunc("GET /events", a.stream)
 	mux.HandleFunc("GET /ping", a.ping)
+	mux.HandleFunc("GET /view/{carUUID}", a.viewCar)
 	mux.HandleFunc("POST /user/login", a.login)
 	mux.HandleFunc("POST /user/challenge", a.challenge) // this endpoint is used after the challenge was solved with the validator, to tell the front end to redirect to dashboard
 	mux.HandleFunc("POST /challenge/{challUUID}", a.challengeValidator)
