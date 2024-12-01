be093ec225937aef996177ce1be69a13c6a04950
canitey <mohamedmed01345@gmail.com>
2024-12-01 23:15:37 +0200

    this function gets the car info
    used when the server request a car by its uuid

--- web/backend/database/cars.go
+++ web/backend/database/cars.go
@@ -14,3 +14,14 @@ func AddCar(d *sql.DB, carInfo models.CarInfo) (error) {
 
 	return nil
 }
+
+// this function gets the cars info from database
+func GetCar(d *sql.DB, carUUID string) (*models.CarInfo, error) {
+	var carinfo models.CarInfo
+	stmt := `SELECT longitude, latitude, driver_status FROM cars WHERE uuid=$1`
+	if err := d.QueryRow(stmt, carUUID).Scan(carinfo.Longitude, carinfo.Latitude, carinfo.DriverStatus); err != nil {
+		return nil, err
+	}
+
+	return &carinfo, nil
+}
