6d1b0eb1cb3bdc4819687378b05fe47f01b75e67
canitey <mohamedmed01345@gmail.com>
2024-12-03 01:32:45 +0200

    started using middlewares

--- web/backend/cmd/web/routes.go
+++ web/backend/cmd/web/routes.go
@@ -1,15 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (a *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	middleware := alice.New(a.sessionManager.LoadAndSave, a.ensureJsonContentType)
 	mux.HandleFunc("GET /challenge/{challUUID}", a.challGetter)
 	mux.HandleFunc("GET /events", a.stream)
 	mux.HandleFunc("GET /ping", a.ping)
 	mux.HandleFunc("GET /view/{carUUID}", a.viewCar)
-	mux.HandleFunc("POST /user/login", a.login)
+	// mux.HandleFunc("POST /user/login", middleware.Then(a.login))
+	mux.Handle("POST /user/login", middleware.ThenFunc(a.login))
 	mux.HandleFunc("POST /user/challenge", a.challenge) // this endpoint is used after the challenge was solved with the validator, to tell the front end to redirect to dashboard
 	mux.HandleFunc("POST /challenge/{challUUID}", a.challengeValidator)
 	mux.HandleFunc("POST /cars/sos", a.sos)
