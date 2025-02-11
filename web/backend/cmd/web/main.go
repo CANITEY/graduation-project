ebd83eeadb32d3707c4b39f8fd2a07c267118fc9
canitey <mohamedmed01345@gmail.com>
2025-02-11 19:48:22 +0200

    fixed a disaster because git

--- web/backend/cmd/web/main.go
+++ web/backend/cmd/web/main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"database/sql"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	"gp-backend/database"
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	"log"
 	"net/http"
 	"time"
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	"github.com/alexedwards/scs/postgresstore"
 	"github.com/alexedwards/scs/v2"
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	_ "github.com/lib/pq"
 	"github.com/r3labs/sse/v2"
 )
@@ -50,18 +37,8 @@ func main() {
 
 
 func OpenDB() (*sql.DB, error) {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	// TODO: connect through ssl (enable sslmode)
 	db, err := sql.Open("postgres", "host=localhost user=emergency password=emergency dbname=cars sslmode=disable")
-=======
-	// BUG: 2024/11/04 00:46:14 pq: SSL is not enabled on the server
-	db, err := sql.Open("postgres", "postgres://emergency:emergency@localhost/cars@sslmode=disable")
->>>>>>> Stashed changes
-=======
-	// BUG: 2024/11/04 00:46:14 pq: SSL is not enabled on the server
-	db, err := sql.Open("postgres", "postgres://emergency:emergency@localhost/cars@sslmode=disable")
->>>>>>> Stashed changes
 	if err != nil {
 		return nil, err
 	}
@@ -70,13 +47,7 @@ func OpenDB() (*sql.DB, error) {
 		return nil, err
 	}
 	
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	log.Println("CONNECTED TO DATABASE CARS")
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 	return db, nil
 }
@@ -91,20 +62,10 @@ func NewApplication(sseParameter string) (*application, error) {
 	server := sse.New()
 	server.CreateStream(sseParameter)
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-	// open database connection
->>>>>>> Stashed changes
-=======
-	// open database connection
->>>>>>> Stashed changes
 	db, err := OpenDB()
 	if err != nil {
 		return nil, err
 	}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	udb := database.UserDB{
 		DB: db,
 	}
@@ -112,21 +73,10 @@ func NewApplication(sseParameter string) (*application, error) {
 	sessionManager := scs.New()
 	sessionManager.Store = postgresstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
 	return &application{
 		sse: server,
 		db: db,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 		udb: udb,
 		sessionManager: sessionManager,
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	}, nil
 }
