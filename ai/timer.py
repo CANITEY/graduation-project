7b73b92173cec5c10dfc5d047810906db985ea11
canitey <mohamedmed01345@gmail.com>
2025-02-14 22:07:46 +0200

    updated face_recognition code

--- ai/timer.py
+++ ai/timer.py
@@ -1,26 +1,46 @@
-#!/bin/env python3
 """
-This is CANITEY MODIFICATION
-GOAL: ADD A TIMER COUNT THAT WILL FIRE EVENT/ACITON IF THE SPAN IS EXCEEDED
+This additions are done by CANITEY
+GOAL:
+    make a sensing for the time span of blink, yawn, eye direction
 """
 import time
 
 
 class Timer:
+    def __init__(self, duration=3, callback=None):
+        self.duration_ = duration
+        self.now_ = time.time()
+        self.current_ = None
+        self.callback_ = self.default_callback if callback == None else callback
+
+    def default_callback(self):
+        print("passed")
+
+    def sense(self):
+        self.current_ = time.time()
+        if self.current_ - self.now_ >= self.duration_ :
+            self.callback_()
+            self.now_ = time.time()
+
+
+
+class GlobalTimer:
     def __init__(self):
-        self.now__ = time.time()
+        self.timers_ = dict()
 
-    def start(self):
-        current = time.time()
-        if current - self.now__ >= 3:
-            print("Nooo")
-        
+    def init_if_none(self, timer: str, duration=3, callback=None):
+        timer_name = timer.lower()
+        if self.timers_.get(timer_name) == None or self.timers_[timer_name] == None:
+            self.timers_[timer_name] = Timer(duration=duration, callback=callback)
+        return self.timers_[timer_name]
+    
+    def reset_all_timers(self):
+        for timer in self.timers_:
+            self.timers_[timer] = None
 
-def main():
-    while True:
-        timer = Timer()
-        timer.start()
+    def reset_except(self, timer: str):
+        for t in self.timers_:
+            if timer.lower() == t.lower():
+                continue
+            self.timers_[t] = None
 
-
-if __name__ == "__main__":
-    main()
